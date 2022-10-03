4f3d32da5315c6abe8cfc34947147788d3d4185c
Agustina Aperlo <43002670+magustinaperlo@users.noreply.github.com>
2022-10-02 21:20:27 -0300

    Update ej5.py
    Correcciones en el código

--- ej5.py
+++ ej5.py
@@ -2,26 +2,32 @@ from tkinter import *
 
 
 def funcionSuma():
-    x1 = int(entry1.get())
-    x2 = int(entry2.get())
-    r = x1 + x1
+    x1 = float(entry1.get())
+    x2 = float(entry2.get())
+    #estamos sumando la misma variable dos veces
+    # r = x1 + x1
+    r=x1+x2
     resultado.set(r)
 
 def funcionResta():
-    x1 = int(entry1.get())
-    x2 = int(entry2.get())
-    r = x1 - x1
+    x1 = float(entry1.get())
+    x2 = float(entry2.get())
+    #estamos restando la misma variable dos veces
+    #r = x1 - x1
+    r= x1-x2
     resultado.set(r)
 
 def funcionProducto():
-    x1 = int(entry1.get())
-    x2 = int(entry2.get())
-    r = x1 * x1
+    x1 = float(entry1.get())
+    x2 = float(entry2.get())
+     #estamos multiplicando la misma variable dos veces
+   # r = x1 * x1
+    r = x1*x2
     resultado.set(r)
 
 def funcionDivision():
-    x1 = int(entry1.get())
-    x2 = int(entry2.get())
+    x1 = float(entry1.get())
+    x2 = float(entry2.get())
     if x2 ==  0:
         r="Error, no se puede dividir por 0."
         resultado.set(r)
@@ -30,68 +36,14 @@ def funcionDivision():
         resultado.set(r)
 
 def funcionPorcentaje():
-    x1 = int(entry1.get())
-    x2 = int(entry2.get())
-    r = x1%x2
+    x1 = float(entry1.get())
+    x2 = float(entry2.get())
+    #error en el calculo de porcentaje, estás sacando el módulo que es otra operación matemática
+    #r = x1%x2 
+    r = (x1*x2)/100
     resultado.set(r)
 
 def funcionClear():
     e1.set("")
     e2.set("")
     resultado.set("")
-
-
-
-
-
-
-window = Tk()
-window.geometry("300x300")
-window.title("Calculadora")
-window.config(bg = "white")
-
-e1= StringVar()
-e2= StringVar()
-resultado= StringVar()
-e1.set("")
-e2.set("")
-resultado.set("")
-
-label1 = Label(window,text= "Primer valor",bg ="lightgrey")
-label1.place(x =30,y = 15)
-entry1 = Entry(window,textvariable=e1)
-entry1.place(x = 150, y = 15)
-
-
-label2 = Label(window,text= "Segundo valor",bg ="lightgrey")
-label2.place(x = 30, y = 50)
-entry2 = Entry(window,textvariable=e2)
-entry2.place(x = 150, y = 50)
-
-label3 = Label(window,text= "Resultado",bg ="lightgrey")
-label3.place(x = 30, y = 90)
-entry3 = Entry(window,textvariable=resultado,state="readonly")
-entry3.place(x = 150, y = 90)
-
-botonSuma = Button(window, text = " + ", command = funcionSuma, bg="lightgrey",width=10)
-botonSuma.place(x = 40, y = 130)
-
-botonResta = Button(window, text = " - ", command = funcionResta, bg="lightgrey",width=10)
-botonResta.place(x = 160, y = 130)
-
-botonMultiplicacion = Button(window, text = " * ", command = funcionProducto, bg="lightgrey",width=10)
-botonMultiplicacion.place(x = 40, y = 170)
-
-botonDivision= Button(window, text = " / ", command = funcionDivision, bg="lightgrey",width=10)
-botonDivision.place(x = 160, y = 170)
-
-botonPorcentaje= Button(window, text = " % ", command = funcionPorcentaje, bg="lightgrey",width=10)
-botonPorcentaje.place(x = 40, y = 210)
-
-botonClear = Button(window, text = " AC ", command = funcionClear, bg="Red",width=10)
-botonClear.place(x = 160, y = 210)
-
-
-
-
-window.mainloop()
